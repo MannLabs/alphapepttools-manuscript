6f10bfdb1b4390821d5543567172bc306fbf64a3
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-09-07 20:02:44 +0200

    [fix] Do not create redundant plot directory

--- notebooks/main/multimodal/data/download.sh
+++ notebooks/main/multimodal/data/download.sh
@@ -8,7 +8,7 @@
 # Lehtiö J, Arslan T, Siavelis I, Pan Y, Socciarelli F, Berkovska O, Umer HM, Mermelekas G, Pirmoradian M, Jönsson M, Brunnström H, Brustugun OT, Purohit KP, Cunningham R, Asl HF, Isaksson S, Arbajian E, Aine M, Karlsson A, Kotevska M, Hansen CG, Haakensen VD, Helland Å, Tamborero D, Johansson HJ, Branca RM, Planck M, Staaf J, Orre LM. Proteogenomics of non-small cell lung cancer reveals molecular subtypes associated with specific therapeutic targets and immune evasion mechanisms. Nat Cancer. 2021 2(11):1224-1242
 #  DOI: 10.1038/s43018-021-00259-9 ; PubMed: 34870237
 
-mkdir -p ./{raw/dda-1,pp,results,models,plots}
+mkdir -p ./{raw/dda-1,pp,results,models}
 
 pridepy download-file-by-name \
     -a PXD020191 \
